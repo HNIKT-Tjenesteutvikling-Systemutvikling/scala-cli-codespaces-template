93647c5b0f4021b2e755edd60cc0d60b1d6d7dca
Kristian Nedrevold-Hansen <kristian@krined.no>
2023-11-29 21:42:51 +0100

    Added direnv

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,4 +7,3 @@ echo "# Custom bash prompt" >> ~/.bashrc
 echo "PS1='\e[0;32m\u\e[0m → \e[0;34m\W\e[0m \$ '" >> ~/.bashrc
 echo "" >> ~/.bashrc
 
-nix develop --extra-experimental-features nix-command --extra-experimental-features flakes
